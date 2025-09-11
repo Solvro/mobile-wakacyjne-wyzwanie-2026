18f0c787630d6d4f5c0e1ac09e85c0dfffde66d7
francesspot <franciszekpora2@gmail.com>
2025-09-12 01:04:34 +0200

    chyba naprawiono photos_repository

--- where_to_go/lib/features/photos/photos_repository.dart
+++ where_to_go/lib/features/photos/photos_repository.dart
@@ -1,8 +1,10 @@
 import "dart:io";
 import "package:dio/dio.dart";
+import "package:logger/logger.dart";
 
 class PhotosRepository {
   final Dio _dio;
+  final _logger = Logger();
 
   PhotosRepository(this._dio);
 
@@ -14,15 +16,25 @@ class PhotosRepository {
     final response = await _dio.post<Map<String, dynamic>>(
       "/photos/upload",
       data: formData,
-      options: Options(contentType: "multipart/form-data"),
+      options: Options(
+        contentType: "multipart/form-data",
+        headers: {
+          "Accept": "application/json",
+        },
+      ),
     );
+
+    _logger.d("Status: ${response.statusCode}");
 
     if (response.statusCode == 200 || response.statusCode == 201) {
       final data = response.data;
-      if (data != null && data.containsKey("url")) {
-        return data["url"] as String;
+      _logger.d("Odpowiedź serwera: $data");
+
+      if (data != null && data.containsKey("filename")) {
+        final filename = data["filename"] as String;
+        return "https://backend-api.w.solvro.pl/photos/$filename";
       } else {
-        throw Exception("Brak URL w odpowiedzi serwera");
+        throw Exception("Brak pola 'filename' w odpowiedzi serwera");
       }
     } else {
       throw Exception("Nie udało się przesłać zdjęcia: ${response.statusCode}");
