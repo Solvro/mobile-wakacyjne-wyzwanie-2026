3daf1861b93139a09d6df48cf347c9f9473c7db9
Patrycjadz0 <patrycjadzielendziak@gmail.com>
2026-08-25 16:06:37 +0200

    Zadanie 2

---
+++ where_to_go/lib/features/places/place.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+class Place {
+  final String id;
+  final String title;
+  final String description;
+  final String shortdesc;
+  final String path;
+  final bool isFavorite;
+  final Color backcolor;
+  final List<String> listastr;
+  final List<Icon> listaicon;
+
+  const Place({
+    required this.id,
+    required this.title,
+    required this.shortdesc,
+    required this.description,
+    this.isFavorite = false,
+    required this.path,
+    required this.backcolor,
+    required this.listastr,
+    required this.listaicon
+  });
+  
+  Place copyWith({bool? isFavorite}) {
+    return Place(
+      id: id,
+      title: title,
+      description: description,
+      path:path,
+      backcolor:backcolor,
+      shortdesc: shortdesc,
+      isFavorite: isFavorite ?? this.isFavorite,
+      listastr: listastr,
+      listaicon: listaicon
+    );
+  }
+}
