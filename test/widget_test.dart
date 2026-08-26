58329a1b998e3484022a8ba0073473edf57e9d90
Kostya-Iva <kostyamega1810@gmail.com>
2026-08-26 18:50:37 +0200

    fix

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:wakacyjne_wyzwanie_26/main.dart';
+import '../where_to_go/lib/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
