0e9519a515bd484c9d1e1103dfbd3378872149b0
kelly034 <d7.mateusz@gmail.com>
2026-08-28 01:53:36 +0200

    fixed formatting

--- where_to_go/lib/app_router.dart
+++ where_to_go/lib/app_router.dart
@@ -20,9 +20,9 @@ final goRouter = GoRouter(
               position: animation.drive(
                 Tween(begin: const Offset(1, 0), end: Offset.zero).chain(CurveTween(curve: Curves.easeOut)),
               ),
-              child: child
+              child: child,
             );
-          }
+          },
         );
       },
     ),
